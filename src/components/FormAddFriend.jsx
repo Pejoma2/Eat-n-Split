a4703a30c43a5aeaac78abe0e20c75d8e84e5e4d
Pedro Martinez <pejoma2@hotmail.com>
2023-08-14 12:30:11 -0500

    FormAddFriend with useState

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,18 +1,47 @@
-import Button from "./Button"
+import { useState } from "react";
+import Button from "./Button";
 
 function FormAddFriend() {
-    return (
-        <form className="form-add-friend">
-            <label>👬Friend Name</label>
-            <input type="text" />
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
 
-            <label>📷Image URL</label>
-            <input type="text" />
+  function handleSubmit(e) {
+    e.preventDefault();
 
-            <Button>Add</Button>
-            
-        </form>
-    )
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    console.log(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>👬Friend Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>📷Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
 }
 
-export default FormAddFriend
+export default FormAddFriend;
